be5f5172351fb19827939424d653c01d11990b1b
Benjamin Ledetzky <benjamin.ledetzky@gmail.com>
2023-10-20 22:52:09 +0200

    updated code. almost done.

--- script.js
+++ script.js
@@ -3,25 +3,22 @@ let images = ['./img/1.jpg', './img/2.jpg', './img/3.jpg', './img/4.jpg', './img
 
 function init(){
     loadHeader();
-    render(); 
+    render();
 }
 
 
 function loadHeader(){
     
-    let content = document.getElementById('content');
-    content.innerHTML =
-   `<header>
-    <h1>Meine Bildergalerie</h1>
-    </header>`;
 }
+
+
 function render(){
  
     let imageBox = document.getElementById('imagebox');
     for (let i = 0; i < images.length; i++){
         const image = images[i];
         imageBox.innerHTML += `
-        <div class="imagebox" onclick="showPicture(${i})">
+        <div class="thumbnailbox" onclick="showPicture(${i})">
         <img src="${images[i]}" class="thumbnail">
         </div>`;
     }
@@ -32,23 +29,37 @@ function showPicture(i){
     let image = images[i]; 
     
     document.getElementById('openImageBox').innerHTML = `
-    
+
     <div class="open-image" onclick="togglePicture()">
-    <div class="arrow left-arrow" id="leftArrow" onclick="showPreviousPicture(${i})">◄</div>
+    ${generateLeftArrow(i)}
     <img src="${image}" >
-    <div class="arrow right-arrow" id="rightArrow" onclick="showNextPicture(${i})">►</div>
+    ${generateRightArrow(i)}
     </div>`;
 }
 
+
+function generateLeftArrow(i){
+    if(i > 0){
+        return `<div class="arrow left-arrow" id="leftArrow" onclick="showPreviousPicture(${i})"><img src="./icons/arrowleft.png"></div>`;
+    }
+    return '';
+}
+
+
+function generateRightArrow(i){
+   return `<div class="arrow right-arrow" id="rightArrow" onclick="showNextPicture(${i})">
+   <img src="./icons/arrowright.png">
+   </div>`;
+}
 
 function togglePicture(){
     document.getElementById('openImageBox').classList.toggle('d-none');
 }
 
 
-function showNextPicture(currentIndex){
-    if (currentIndex < images.length -1){
-        showPicture(currentIndex +1);
+function showNextPicture(currentIndex) {
+    if (currentIndex < images.length - 1) {
+        showPicture(currentIndex + 1);
     }
 }
 
